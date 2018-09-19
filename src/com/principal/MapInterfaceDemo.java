875fc2eb343ddcfc2661248699bb1e7d2c38f4e3
root <root@java-System-Product-Name>
2018-09-19 12:39:11 +0200

    Version1 a medias.

---
+++ src/com/principal/MapInterfaceDemo.java
@@ -0,0 +1,20 @@
+package com.principal;
+
+import java.util.Map;
+
+public class MapInterfaceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Map<String, Integer> m;	
+	}
+
+}
+
+//un mapa es un objeto que mapea o selecciona claves con valores donde las claves no pueden estar duplicadas
+
+//contar frecuencia de ocurriencia de cada una de las palabras que se pasan como parametro en el array args
+//crear un mapa (map objet o map interface) que en las claves tengan las palabras y en los valores la frecuencia de ocurrencia de
+//dichas palabras
+
